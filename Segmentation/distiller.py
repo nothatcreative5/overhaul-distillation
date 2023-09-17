60df9761926a4f01998997f41842d5ed93d28e42
unknown <nothatcreative5@gmail.com>
2023-09-18 01:17:35 +0330

    PCA

--- Segmentation/distiller.py
+++ Segmentation/distiller.py
@@ -94,35 +94,44 @@ class Distiller(nn.Module):
 
         'Corrected ICKD'
 
-        y_cpy = y.clone().detach()
-        # y_cpy = torch.rand((b, h, w), device = 'cuda')
-        y_cpy[y_cpy == 255] = 0
+        # y_cpy = y.clone().detach()
+        # # y_cpy = torch.rand((b, h, w), device = 'cuda')
+        # y_cpy[y_cpy == 255] = 0
 
-        b, c, h, w = s_out.shape
+        # b, c, h, w = s_out.shape
 
-        s_logit = torch.reshape(s_out, (b, c, h*w))
-        t_logit = torch.reshape(t_out, (b, c, h*w)).detach()
+        # s_logit = torch.reshape(s_out, (b, c, h*w))
+        # t_logit = torch.reshape(t_out, (b, c, h*w)).detach()
 
-        y_cpy = torch.reshape(y_cpy, (b, h*w))
+        # y_cpy = torch.reshape(y_cpy, (b, h*w))
 
-        for i in range(b):
-            preds = torch.argmax(t_logit[i], dim = 0)
-            indices = y_cpy[i] != preds
-            val_mx = torch.max(t_logit[i]).detach()
-            val_mn = torch.min(t_logit[i]).detach()
+        # for i in range(b):
+        #     preds = torch.argmax(t_logit[i], dim = 0)
+        #     indices = y_cpy[i] != preds
+        #     val_mx = torch.max(t_logit[i]).detach()
+        #     val_mn = torch.min(t_logit[i]).detach()
 
-            corrected_logits = torch.ones((c, indices.sum()), device = 'cuda') * val_mn
-            corrected_logits[y_cpy.long()[i][indices], torch.arange(indices.sum())] = val_mx
-            t_logit[i][:, indices] = corrected_logits
+        #     corrected_logits = torch.ones((c, indices.sum()), device = 'cuda') * val_mn
+        #     corrected_logits[y_cpy.long()[i][indices], torch.arange(indices.sum())] = val_mx
+        #     t_logit[i][:, indices] = corrected_logits
 
-        # b x c x A  mul  b x A x c -> b x c x c
-        ICCT = torch.bmm(t_logit, t_logit.permute(0,2,1))
-        ICCT = torch.nn.functional.normalize(ICCT, dim = 2)
+        # # b x c x A  mul  b x A x c -> b x c x c
+        # ICCT = torch.bmm(t_logit, t_logit.permute(0,2,1))
+        # ICCT = torch.nn.functional.normalize(ICCT, dim = 2)
 
-        ICCS = torch.bmm(s_logit, s_logit.permute(0,2,1))
-        ICCS = torch.nn.functional.normalize(ICCS, dim = 2)
+        # ICCS = torch.bmm(s_logit, s_logit.permute(0,2,1))
+        # ICCS = torch.nn.functional.normalize(ICCS, dim = 2)
 
-        G_diff = ICCS - ICCT
-        loss_distill = (G_diff * G_diff).view(b, -1).sum() / (c)
+        # G_diff = ICCS - ICCT
+        # loss_distill = (G_diff * G_diff).view(b, -1).sum() / (c)
+
+        'Unsupervised loss'
+
+        b,c,h,w = t_feats[3].shape
+        s_feats[3] = self.Connectors[3](s_feats[3])
+        U_S, _, _ = torch.pca_lowrank(s_feats[3].view(b, c, -1), q = 21)
+        U_T, _, _ = torch.pca_lowrank(t_feats[3].view(b, c, -1).detach(), q = 21)
+
+        loss_distill = (U_S - U_T).pow(2).mean()
 
         return s_out, loss_distill
--- Segmentation/train_with_distillation.py
+++ Segmentation/train_with_distillation.py
@@ -121,7 +121,7 @@ class Trainer(object):
             output, loss_distill = self.d_net(image, target)
 
             loss_seg = self.criterion(output, target)
-            loss = loss_seg + loss_distill.sum() / batch_size * 0.1
+            loss = loss_seg + loss_distill.sum() / batch_size
 
             loss.backward()
             optimizer.step()
