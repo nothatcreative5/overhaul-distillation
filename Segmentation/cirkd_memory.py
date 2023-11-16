cfce08928fb6f3987f452e50cdfc919b07a36c62
unknown <nothatcreative5@gmail.com>
2023-11-16 20:28:16 +0330

    ss

--- Segmentation/cirkd_memory.py
+++ Segmentation/cirkd_memory.py
@@ -63,12 +63,12 @@ class StudentSegContrast(nn.Module):
         Performs all_gather operation on the provided tensors.
         *** Warning ***: torch.distributed.all_gather has no gradient.
         """
-        tensors_gather = [torch.ones_like(tensor)
-            for _ in range(torch.distributed.get_world_size())]
-        torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
+        # tensors_gather = [torch.ones_like(tensor)
+            # for _ in range(torch.distributed.get_world_size())]
+        # torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
 
-        output = torch.cat(tensors_gather, dim=0)
-        return output
+        # output = torch.cat(tensors_gather, dim=0)
+        return tensor
 
     
     def _dequeue_and_enqueue(self, keys, labels):
--- Segmentation/cirkd_mini_batch.py
+++ Segmentation/cirkd_mini_batch.py
@@ -47,12 +47,12 @@ class CriterionMiniBatchCrossImagePair(nn.Module):
         Performs all_gather operation on the provided tensors.
         *** Warning ***: torch.distributed.all_gather has no gradient.
         """
-        tensors_gather = [torch.ones_like(tensor)
-            for _ in range(torch.distributed.get_world_size())]
-        torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
+        # tensors_gather = [torch.ones_like(tensor)
+            # for _ in range(torch.distributed.get_world_size())]
+        # torch.distributed.all_gather(tensors_gather, tensor, async_op=False
 
-        output = torch.cat(tensors_gather, dim=0)
-        return output
+        # output = torch.cat(tensors_gather, dim=0)
+        return tensor
 
 
     def forward(self, feat_S, feat_T):
