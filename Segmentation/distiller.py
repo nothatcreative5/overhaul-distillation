b3d2888afe6f73289c46dbbe3c4ed373f877a9b9
unknown <nothatcreative5@gmail.com>
2023-12-31 17:09:24 +0330

    cbam only

--- Segmentation/distiller.py
+++ Segmentation/distiller.py
@@ -93,7 +93,7 @@ class Distiller(nn.Module):
             b,c,h,w = t_feats[i].shape
             M = h * w
             s_feats[i] = self.Connectors[i](self.attns[i-3](s_feats[i])).view(b, c, -1)
-            # t_feats[i] = CBAM(t_feats[i].shape[1], model = 'teacher').cuda()(t_feats[i]).view(b, c, -1).detach()
+            t_feats[i] = CBAM(t_feats[i].shape[1], model = 'teacher').cuda()(t_feats[i]).view(b, c, -1).detach()
 
 
             # s_feats[i] = self.Connectors[i](s_feats[i])
